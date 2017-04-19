bfb3706225e983f6c9ea52cbd622a0ff17b7acd9
Haisong Ye <ye.haisong@gmail.com>
2017-04-19 15:56:18 -0400

    Preliminary analysis for SR and ST when r=0

---
+++ ES_SR_ST_r0.r
@@ -0,0 +1,32 @@
+#Shortterm Retention and shortterm Transfer effect sizes
+library(readr)
+library(metaSEM)
+library(metafor)
+
+##Read data from csv
+SRSTd <- read_csv("SRd_STd_r0.csv")
+
+##Multivariance meta analysis with metaSEM
+##SRd-short term retention effect size, STd-short tem transfer effect size, v-variance,cov-covariance
+result<-meta(y=cbind(SRd,STd),v=cbind(SRv,SRSTcov,STv),data=SRSTd, model.name="Random effects model")
+summary(result)
+
+##plot the effect sizes and their confidence ellipses
+plot(result,diag.panel = FALSE)
+
+##Plot the effect sizes with the forest plots
+##create extra panels for the forest plots
+plot(result,diag.panel = TRUE, 
+     main="Learning Performance Analysis", 
+     axis.labels = c("Retention","Transfer"))
+##forest plot for retention
+forest(rma(yi=SRd,vi=SRv,data=SRSTd))
+title("Forest plot of retention")
+##forest plot for transfer
+forest(rma(yi=STd,vi=STv,data=SRSTd))
+title("Forest plot of transfer")
+
+##three moderators
+result1<-meta(y=cbind(SRd,STd),v=cbind(SRv,SRSTcov,STv),data=SRSTd, model.name="Random effects model",x=cbind(PP,LVS,ToV))
+summary(result1)
+plot(result1)
