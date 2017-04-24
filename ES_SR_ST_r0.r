64fcc8f8978365938b403cecfdd018815cf65b57
yehaisong <yehaisong@hotmail.com>
2017-04-24 12:25:02 -0400

    Normality test added
    qqnorm plots were added. but normality test results were not found

--- ES_SR_ST_r0.r
+++ ES_SR_ST_r0.r
@@ -2,9 +2,27 @@
 library(readr)
 library(metaSEM)
 library(metafor)
+library(MVN)
 
 ##Read data from csv
 SRSTd <- read_csv("SRd_STd_r0.csv")
+
+##QQPlot test using metaFor
+resSR<-rma(measure="SMD",SRd,SRv,data=SRSTd)
+qqnorm(resSR,label="out", main="Normal Q-Q Plot of Retention")
+resSR1M<-rma(measure="SMD",SRd,SRv,mods=cbind(PP,LVS,ToV),data=SRSTd)
+qqnorm(resSR1M,label="out", main="Normal Q-Q Plot of Retention with Moderators")
+
+resST<-rma(measure="SMD",STd,STv,data=SRSTd)
+qqnorm(resST,label="out",main="Normal Q-Q Plot of Transfer")
+resST1M<-rma(measure="SMD",STd,STv,mods=cbind(PP,LVS,ToV),data=SRSTd)
+qqnorm(resST1M,label="out",main="Normal Q-Q Plot of Trasnfer with Moderators")
+
+resCL<-rma(measure="SMD",CLd,CLv,data=SRSTd)
+qqnorm(resCL)
+
+##Normality test using Maridia's MVN
+uniPlot(SRSTd[9:10],type="qqplot")
 
 ##Multivariance meta analysis with metaSEM
 ##SRd-short term retention effect size, STd-short tem transfer effect size, v-variance,cov-covariance
