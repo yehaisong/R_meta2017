f4f2ea888049a6464e28ea19de71161dd451c4c7
Haisong Ye <ye.haisong@gmail.com>
2017-04-24 16:19:49 -0400

    Normailty statistic test added

--- ES_SR_ST_r0.r
+++ ES_SR_ST_r0.r
@@ -3,18 +3,24 @@ library(readr)
 library(metaSEM)
 library(metafor)
 library(MVN)
+library(nortest)
 
 ##Read data from csv
 SRSTd <- read_csv("SRd_STd_r0.csv")
 
-<<<<<<< HEAD
 ##QQPlot test using metaFor
 resSR<-rma(measure="SMD",SRd,SRv,data=SRSTd)
+srd<-SRSTd[["SRd"]]
+lillie.test(srd)#Kolmogorov-Smirnov normality test
+sf.test(srd)#Shapiro-Francia normality test
 qqnorm(resSR,label="out", main="Normal Q-Q Plot of Retention")
 resSR1M<-rma(measure="SMD",SRd,SRv,mods=cbind(PP,LVS,ToV),data=SRSTd)
 qqnorm(resSR1M,label="out", main="Normal Q-Q Plot of Retention with Moderators")
 
 resST<-rma(measure="SMD",STd,STv,data=SRSTd)
+std<-SRSTd[["STd"]]
+lillie.test(std)#Kolmogorov-Smirnov normality test
+sf.test(std)#Shapiro-Francia normality test
 qqnorm(resST,label="out",main="Normal Q-Q Plot of Transfer")
 resST1M<-rma(measure="SMD",STd,STv,mods=cbind(PP,LVS,ToV),data=SRSTd)
 qqnorm(resST1M,label="out",main="Normal Q-Q Plot of Trasnfer with Moderators")
@@ -24,11 +30,9 @@ qqnorm(resCL)
 
 ##Normality test using Maridia's MVN
 nmtres<-mardiaTest(SRSTd[,c(9:10)],qqplot=TRUE)
-uniPlot(SRSTd[9:10],type="qqplot")
+unires<-uniPlot(SRSTd[9:10],type="qqplot")
 
-=======
 ##r=0
->>>>>>> origin/master
 ##Multivariance meta analysis with metaSEM
 ##SRd-short term retention effect size, STd-short tem transfer effect size, v-variance,cov-covariance
 result<-meta(y=cbind(SRd,STd),v=cbind(SRv,SRSTcov,STv),data=SRSTd, model.name="Random effects model")
@@ -42,7 +46,6 @@ cov2cor(T2)
 
 
 ##plot the effect sizes and their confidence ellipses
-plot(result,diag.panel=FALSE)
 plot(result, axis.labels = c("Retention","Transfer"))
 
 ##Plot the effect sizes with the forest plots
@@ -57,8 +60,6 @@ title("Forest plot of retention")
 forest(rma(yi=STd,vi=STv,data=SRSTd))
 title("Forest plot of transfer")
 
-##three moderators
-resultall<-meta(y=cbind(SRd,STd),v=cbind(SRv,SRSTcov,STv),data=SRSTd, model.name="Random effects model",x=cbind(PP,LVS,ToV))
 
 ##r=1
 result1<-meta(y=cbind(SRd,STd),v=cbind(SRv,SRSTcov1,STv),data=SRSTd, model.name="Random effects model")
